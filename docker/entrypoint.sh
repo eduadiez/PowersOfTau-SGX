195064c2d8395d55e5b59df6b9ba03f940921186
Eduardo Antuña <eduadiez@gmail.com>
2019-11-14 12:03:04 +0100

    Improve build command

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "--command $@ $PWD"
-
-. /home/user/.nix-profile/etc/profile.d/nix.sh && nix-shell /home/user/shell.nix --command "$@"
+COMMAND=$@
+source /home/user/.nix-profile/etc/profile.d/nix.sh && nix-shell /home/user/shell.nix --command "$COMMAND"
